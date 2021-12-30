dd856637073585f1343d623a142c28c0deb840c7
Said Oudouane <saidoudouane@gmail.com>
2021-12-30 20:26:54 +0100

    add default reservation vehicle and fix nav reserve btn css not being applied

--- dist/scripts/reserve.js
+++ dist/scripts/reserve.js
@@ -1,9 +1,7 @@
 const search = document.location.search;
-const id = new URLSearchParams(search).get("id");
+const id = new URLSearchParams(search).get("id") ?? 0;
 
 const reserve = document.querySelector(".reserve");
 
-if (id) {
-  const { type, model, name } = vehiclesArr[id];
-  reserve.textContent = `this is page for ${id} vehicle ${type} - ${model} - ${name}`;
-}
+const { type, model, name } = vehiclesArr[id];
+reserve.textContent = `this is page for ${id} vehicle ${type} - ${model} - ${name}`;
