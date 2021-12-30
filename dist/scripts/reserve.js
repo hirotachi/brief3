79d3a4dfeda6a992a142b1b1baeeba201af7964a
Said Oudouane <saidoudouane@gmail.com>
2021-12-30 22:36:02 +0100

    add responsive reservation page intro and layout planning

--- dist/scripts/reserve.js
+++ dist/scripts/reserve.js
@@ -4,4 +4,4 @@ const id = new URLSearchParams(search).get("id") ?? 0;
 const reserve = document.querySelector(".reserve");
 
 const { type, model, name } = vehiclesArr[id];
-reserve.textContent = `this is page for ${id} vehicle ${type} - ${model} - ${name}`;
+// reserve.textContent = `this is page for ${id} vehicle ${type} - ${model} - ${name}`;
