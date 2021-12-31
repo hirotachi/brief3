9229f84fe9b473ae4edfd2ea19a1769edfb986d7
Said Oudouane <saidoudouane@gmail.com>
2021-12-31 20:14:42 +0100

    add logic to calculate duration between dates and initial layout for checkout form

--- dist/scripts/helpers.js
+++ dist/scripts/helpers.js
@@ -25,3 +25,17 @@ class State {
 function getID() {
   return Math.random().toString(16).slice(2) + Date.now().toString(16);
 }
+
+function parseDate(date) {
+  const year = date.getFullYear();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  return `${year}-${month.toString().padStart(2, "0")}-${day
+    .toString()
+    .padStart(2, "0")}`;
+}
+
+function diffInDays(startDate, endDate) {
+  const diffInTime = endDate.getTime() - startDate.getTime();
+  return Math.round(diffInTime / (1000 * 3600 * 24));
+}
--- dist/scripts/reserve.js
+++ dist/scripts/reserve.js
@@ -14,14 +14,56 @@ infoType.textContent = `${type} - ${model}`;
 infoImg.src = `assets/vehicles/${img}.png`;
 infoImg.alt = `${model} ${name}`;
 
-const inputs = document.querySelector(".inputs");
+const fuel = document.querySelector(".fuel");
+const startDateInput = document.querySelector("#start");
+const endDateInput = document.querySelector("#end");
 
-const checkoutState = new State({
-  fuel: undefined,
-  gearBox: undefined,
-  duration: 1,
-});
+const checkoutState = new State(
+  {
+    fuel: undefined,
+    duration: 1,
+    startDate: new Date(),
+    endDate: getAfterDays(new Date(), 1),
+  },
+  handleStateChange
+);
+handleStateChange(checkoutState.state);
+
+function handleStateChange(state) {
+  setEndDate(state);
+  checkoutState.state.duration = diffInDays(state.startDate, state.endDate);
+  //   change summary text to indicate new stuff
+}
+
+function getAfterDays(date, days = 1) {
+  const copy = new Date(date);
+  return new Date(copy.setHours(copy.getHours() + days * 24));
+}
+
+startDateInput.min = parseDate(checkoutState.state.startDate);
+startDateInput.value = parseDate(checkoutState.state.startDate);
+
+function setEndDate(state) {
+  const { startDate, endDate } = state;
+  if (!startDate || !endDate) return;
+  const nextDay = getAfterDays(startDate);
+  const date = parseDate(nextDay);
+  endDateInput.min = date;
+  if (parseDate(endDate) > parseDate(startDate)) return;
+  endDateInput.value = date;
+  checkoutState.state.endDate = nextDay;
+}
 
 // build fuel select
 // build gear box options
 // duration input
+
+// events listeners
+const handleDateChange = (e) => {
+  checkoutState.state = {
+    ...checkoutState.state,
+    [e.target.name]: e.target.valueAsDate,
+  };
+};
+startDateInput.addEventListener("input", handleDateChange);
+endDateInput.addEventListener("input", handleDateChange);
