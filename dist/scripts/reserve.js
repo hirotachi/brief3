779630ab052d9e88cb0bb6395361126ab1de1ddc
Said Oudouane <saidoudouane@gmail.com>
2021-12-31 18:09:20 +0100

    remove built files to avoid conflicts

--- dist/scripts/reserve.js
+++ dist/scripts/reserve.js
@@ -6,11 +6,22 @@ const infoType = reserve.querySelector(".info__type");
 const infoName = reserve.querySelector(".info__name");
 const infoImg = reserve.querySelector(".info__preview img");
 
-const { type, model, name } = vehiclesArr[id];
+const { type, model, name } = vehiclesArr[id] ?? vehiclesArr[0];
 const img = `${model} ${name}`.replace(/\s+/g, "_").toUpperCase();
 
 infoName.textContent = name;
 infoType.textContent = `${type} - ${model}`;
 infoImg.src = `assets/vehicles/${img}.png`;
 infoImg.alt = `${model} ${name}`;
-// reserve.textContent = `this is page for ${id} vehicle ${type} - ${model} - ${name}`;
+
+const inputs = document.querySelector(".inputs");
+
+const checkoutState = new State({
+  fuel: undefined,
+  gearBox: undefined,
+  duration: 1,
+});
+
+// build fuel select
+// build gear box options
+// duration input
